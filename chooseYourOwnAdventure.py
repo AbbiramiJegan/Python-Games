46c198d1da86389a24a0037fc6eb294f1ec82bd0
AbbiramiJegan <108280861+AbbiramiJegan@users.noreply.github.com>
2023-06-25 20:20:55 +0800

    Created the first 5 pages of the story

---
+++ chooseYourOwnAdventure.py
@@ -0,0 +1,13 @@
+name = input("Type your name: ")
+print("Welcome", name, "to this adventure!")
+
+answer = input(
+    "Ben liked playing a pickup game of basketball after school. Being in middle school, he spent a lot of the day sitting at a desk, so an hour or so running up and down the court gave him energy. When the game was over, he grabbed his sweatshirt and wiped the sweat from his neck. 'Where are you going?'\n"
+    "'Got to run home to shower before I pick up my little sister from her afterschool program,' Ben told his friend Mikey as he made his way off the basketball court.\n\n"
+    "Ben headed toward his apartment building, listening to music. When he rounded the corner, Ben saw that his street was lined with City trucks, utility vehicles, orange cones, and wooden barriers. A police cruiser was parked behind one of the barriers, its lights flashing. The garbled sound of two-way radios was everywhere. And a strange, rushing, whooshing made the block sound less like the city and more like a riverside. What in the world was going on? Ben climbed the front stairs of a neighboring building to get a better look.\n"
+    "In the middle of the intersection, by the entrance to his apartment building, a burbling, gurgling mound of water seemed to boil up out of the street. Water was rushing everywhere. 'Kid! Hey, kid!' Ben looked down and saw a man in a green vest pointing up at him with a walkie-talkie. 'You live in one of these buildings? I’m with the Community Emergency Response Team,' the man with the walkie-talkie said, holding up his ID badge. 'See, it’s written as ‘CERT’ on my badge. The name’s O’Meara.'\n"
+    "'What’s going on, Mr. O’Meara?' Ben asked. '\n"
+    "and ben can go to a friend's house or can go up to his apartment."
+)
+
+
